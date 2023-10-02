935913084bcf839af747ac5d1a082a881814c756
Vova iz Tambova <voller.ruden@yandex.ru>
2023-10-02 20:50:46 +0300

    Краш-тест сервера

--- backend/app.js
+++ backend/app.js
@@ -29,6 +29,12 @@ app.use(apiLimiter);
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.post('/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
